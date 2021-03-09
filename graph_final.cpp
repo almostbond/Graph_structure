4344078540956eda218ee56c1a807c82778c8ebe
almostbond <48605575+almostbond@users.noreply.github.com>
2021-03-08 17:30:53 -0800

    Add files via upload
    everything currently works. working on dijkstra_helper it currently only gets the lowest cost path.

--- graph_final.cpp
+++ graph_final.cpp
@@ -44,22 +44,17 @@ void Graph::create_vertex(string name) {
     else {
         cout << index << endl;
         array_content[index]->vertex_name = name;
-       
-        
-        
-        
         cout << "vertex "<< array_content[index]->vertex_name <<" created" << endl;
         index++;
     }
 }
 
 
-
 void Graph::create_edge(int weight, int ipos, int fpos) {
+
     vertex* source = array_content[ipos];
     vertex* destination = array_content[fpos];
     edge* next_edge = array_content[ipos]->edge_ptr;
-    
 
     edge* new_edge = new edge;
     new_edge->edge_weight = weight;
@@ -67,17 +62,17 @@ void Graph::create_edge(int weight, int ipos, int fpos) {
 
     if (next_edge != NULL) {
         
-        while (next_edge != NULL) {
+        while (next_edge->edge_next != NULL) {
             next_edge = next_edge->edge_next;
         }
-       
+        next_edge->edge_next = new_edge;
+        new_edge->vertex_ptr = destination;
     }
+    else {
 
-    source->edge_ptr = new_edge;
-    new_edge->vertex_ptr = destination;
-
-  
-        
+        source->edge_ptr = new_edge;
+        new_edge->vertex_ptr = destination;
+    }    
 
     cout << "edge between "<< array_content[ipos]->vertex_name << " and " << destination->vertex_name<<" made" << endl;
 
@@ -92,12 +87,11 @@ void Graph::add_edge(int weight, int ipos, int fpos) {
 
 void Graph::print_adjacent(int sauce) {
 
-
     cout << "nodes adjacent to " << array_content[sauce]->vertex_name << endl;
-    //cout << "test   " << array_content[sauce]->edge_ptr->vertex_ptr->vertex_name << endl;
+    
     edge* source = array_content[sauce]->edge_ptr;
-   
-    while (source != NULL) {       
+
+    while (source != NULL) {
 
         if (source->vertex_ptr != NULL) {
             cout << source->vertex_ptr->vertex_name << " ";
@@ -110,12 +104,31 @@ void Graph::print_adjacent(int sauce) {
 
 void Graph::dijkstra(int ipos, int fpos) {
 
+    dijkstra_helper(ipos, fpos);
+
 }
 
-void Graph::dijkstra_helper(int ipos, int fpos) {
-   // node* source = array_content[ipos];
-   // node* destination = array_content[fpos];
+edge* Graph::dijkstra_helper(int ipos, int fpos) {
+   
+    cout << "adjacent edge values for "<< array_content[ipos]->vertex_name << ": ";
+    
+    int smallest_weight = 100;
 
+    edge* source = array_content[ipos]->edge_ptr;
+
+    while (source != NULL) {
+
+        if (source->vertex_ptr != NULL) {
+            cout << source->edge_weight << " ";
+
+            if (smallest_weight > source->edge_weight) {
+                smallest_weight = source->edge_weight;
+            }
+            
+        }
+        source = source->edge_next;
+    }
+    cout << endl;
 
 
 
@@ -148,17 +161,20 @@ void Graph_test::create_edge_test() {
     add_edge(5,3,4);// D-E
     add_edge(6,1,4);// B-E
     add_edge(7,2,4);// C-E
-    
-   
 
     print_adjacent(0);
-   // print_adjacent(1);
-   // print_adjacent(2);
-   // print_adjacent(3);
-   // print_adjacent(4);
+    print_adjacent(1);
+    print_adjacent(2);
+    print_adjacent(3);
+    print_adjacent(4);
+   
+}
+
+void Graph_test::dijkstra_test() {
+    create_edge_test();
+    dijkstra(0,2);
 
 
-    
 }
 
 int main()
@@ -167,7 +183,9 @@ int main()
     Graph_test test;
 
     //test.create_vertex_test();
-    test.create_edge_test();
+    //test.create_edge_test();
+    test.dijkstra_test();
+
     cout << "\nexit success.\n";
 }
 
